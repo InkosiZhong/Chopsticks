17bf1b1271fb0428fa6c65bd522896371e5e918f
InkosiZhong <inkosizhong@gmail.com>
2023-05-03 16:53:33 +0800

    format setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,29 @@
-pip install pyinstaller
-pip install tabulate
-pip install psutil 
+echo Configuring environment
+pip install pyinstaller >> setup_log.txt 2>&1
+pip install tabulate >> setup_log.txt 2>&1
+pip install psutil >> setup_log.txt 2>&1
 
 touch /tmp/chopsticks_pipe.in
 touch /tmp/chopsticks_pipe.out
 
-cquit # avoid chopsticks is running
-pyinstaller -F src/guard.py 
-pyinstaller -F src/credirect.py 
-pyinstaller -F src/csubmit.py 
-pyinstaller -F src/ccancel.py 
-pyinstaller -F src/cls.py 
-pyinstaller -F src/cclean.py 
-pyinstaller -F src/cquit.py 
+echo Terminate the running program
+cquit -f # avoid chopsticks is running
+echo Building chopsticks
+pyinstaller -F src/guard.py >> setup_log.txt 2>&1
+echo Setup service: credirect
+pyinstaller -F src/credirect.py >> setup_log.txt 2>&1
+echo Setup service: csubmit
+pyinstaller -F src/csubmit.py >> setup_log.txt 2>&1
+echo Setup service: ccancel
+pyinstaller -F src/ccancel.py >> setup_log.txt 2>&1
+echo Setup service: cls
+pyinstaller -F src/cls.py >> setup_log.txt 2>&1
+echo Setup service: cclean
+pyinstaller -F src/cclean.py >> setup_log.txt 2>&1
+echo Setup service: cquit
+pyinstaller -F src/cquit.py >> setup_log.txt 2>&1
 mv dist/guard dist/chopsticks
+
+echo Install chopsticks
 sudo cp dist/* /usr/local/bin/
+echo Finish setup, check more information in setup_log.txt
